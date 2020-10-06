7b91cedb5ff56a0f5ede4c6347566637c71d98ea
Cody <ydoc118@gmail.com>
2020-10-06 16:29:15 -0400

    Updates

---
+++ precache-manifest.66e2cad815961f6eb712a55cc0beec4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaed3a2b83b6ee12b4b95b133348826",
+    "url": "/employee_directory/index.html"
+  },
+  {
+    "revision": "9400097cb5df22f0eea4",
+    "url": "/employee_directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "450f5afd3fe8fb42f3a2",
+    "url": "/employee_directory/static/js/2.6f52c6d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee_directory/static/js/2.6f52c6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9400097cb5df22f0eea4",
+    "url": "/employee_directory/static/js/main.e0a96dde.chunk.js"
+  },
+  {
+    "revision": "498fd09be4c9f38769b0",
+    "url": "/employee_directory/static/js/runtime-main.792f3a3a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employee_directory/precache-manifest.cac73ee96378bb0771e0cccda30e2e0f.js"
+  "/employee_directory/precache-manifest.66e2cad815961f6eb712a55cc0beec4d.js"
 );
 
 self.addEventListener('message', (event) => {
